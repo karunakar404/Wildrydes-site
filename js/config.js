f07e7f6ec2c7ce37007718876fb917acad1321ce
karunakar404 <103353333+karunakar404@users.noreply.github.com>
2024-11-08 12:51:45 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_ZIASQSOfa', // e.g., us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_ZIASQSOfa', // e.g., us-east-2_uXboG5pAb
         userPoolClientId: '2gaulsu6tvmaid5rcpp9jr9his', // e.g., 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // Add the AWS region here
+        region: 'us-east-1' // Add the AWS region here
     },
     api: {
-        invokeUrl: 'https://your-api-id.execute-api.us-east-2.amazonaws.com/prod' // Replace with your actual API Gateway invoke URL
+        invokeUrl: 'https://your-api-id.execute-api.us-east-1.amazonaws.com/prod' // Replace with your actual API Gateway invoke URL
     }
 };
